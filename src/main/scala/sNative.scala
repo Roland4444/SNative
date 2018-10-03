5457fe5cadffc93317a8645f654645edd866e8f0
Roland4444 <turbopollux@rambler.ru>
2018-10-03 16:02:02 +0000

    Need to inject Struct Class and Test

--- src/main/scala/sNative.scala
+++ src/main/scala/sNative.scala
@@ -3,13 +3,24 @@ object runner extends App{
   import HelloWorld.CLibrary
   import com.sun.jna.Library
 
-   object MyLibrary {
+  object MyLibrary {
     val INSTANCE: MyLibrary = Native.loadLibrary("HelloLibrary", classOf[MyLibrary]).asInstanceOf[MyLibrary]
   }
 
   trait MyLibrary extends Library {
     def SayHello(a: String, b: String ): Unit
   }
-  var i = 0
+
+  object ebsLibrary {
+    val INSTANCE: ebsLibrary = Native.loadLibrary("cv", classOf[ebsLibrary]).asInstanceOf[ebsLibrary]
+  }
+
+  trait ebsLibrary extends Library {
+    def v_create_session(Session: Object , path: String); Boolean
+
+  }
+
   MyLibrary.INSTANCE.SayHello("Roman", "Bro!")
+  print(ebsLibrary.INSTANCE.v_create_session(AnyRef, "/home/roland/Downloads/download/.build_l64/tests_data/cv_configuration.json"))
+
 }
