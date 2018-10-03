b2147c5ee3b5c515c7774ac86273b95e99e568af
Roland4444 <turbopollux@rambler.ru>
2018-10-03 16:34:22 +0000

    Need to send struct JNA

--- src/main/scala/sNative.scala
+++ src/main/scala/sNative.scala
@@ -1,7 +1,28 @@
+import java.util
+
+import com.sun.jna.Structure
+
 object runner extends App{
   import com.sun.jna.Native
   import HelloWorld.CLibrary
   import com.sun.jna.Library
+
+  import com.sun.jna.Structure
+
+  object Example5Struct {
+
+  class Version extends Structure{
+    override def getFieldOrder = {
+      util.Arrays.asList( {"major"; "minor"; "build"})
+    }
+    var major : Int
+    var minor : Int
+    var build : Int
+  }
+
+  class Session extends Structure{
+    override def getFieldOrder: util.List[String] = ???
+  }
 
   object MyLibrary {
     val INSTANCE: MyLibrary = Native.loadLibrary("HelloLibrary", classOf[MyLibrary]).asInstanceOf[MyLibrary]
